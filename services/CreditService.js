16080c8286843c366a967906ba68daf8a4ebea25
mubariz@deepnucleus.org <mubariz@deepnucleus.org>
2022-10-04 15:31:37 +0530

    create credit service for free credits

--- services/CreditService.js
+++ services/CreditService.js
@@ -9,10 +9,16 @@ module.exports = class CreditService {
       const user = await User.findOne({ _id: userId });
       if (user) {
         console.log("inside credit service");
-        console.log(bodyData,userId);
         const newCredit = await  Credit.create({
           user_id: userId,
-         
+          free_credits_info : {
+            count : 200 , 
+            status : bodyData.status,
+            allocation : bodyData.allocation , 
+            referral_Id : bodyData.referral_Id ,
+            expires_on : '22-2-2023',
+            allocated_on :"12-1-2022"
+          } 
         });
         console.log(newCredit);
         return newCredit;
