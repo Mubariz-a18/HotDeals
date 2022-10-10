bb6385e805347b0709a867bfeb5115b4c4953952
mubariz@deepnucleus.org <mubariz@deepnucleus.org>
2022-10-10 12:23:39 +0530

    complaint route error handling and mix panel with update complaint issue of $set

--- models/complaintSchema.js
+++ models/complaintSchema.js
@@ -3,24 +3,26 @@ const mongoose = require("mongoose");
 const complaintSchema = mongoose.Schema({
   user_id: mongoose.Schema.Types.ObjectId,
   complaint: [{
-    complaint_id: {
-      type: mongoose.Schema.Types.ObjectId
-    },
-    ad_id: {
-      type: mongoose.Schema.Types.ObjectId,
-    },
-    reason: String,
-    complaint_date: String,
-    description: String,
-    attachment: {
-      type: Array,
-      default: [],
-    },
-    status: {
-      type: String,
-      enum: ['OPEN', "PENDING", "RESOLVED"],
-      default: "OPEN"
-    }
+        complaint_id: {
+          type: mongoose.Schema.Types.ObjectId
+        },
+        _id:false,
+        ad_id: {
+          type: mongoose.Schema.Types.ObjectId,
+        },
+        reason: String,
+        complaint_date: String,
+        complaint_updated_date:String,
+        description: String,
+        attachment: {
+          type: Array,
+          default: [],
+        },
+        status: {
+          type: String,
+          enum: ['OPEN', "PENDING", "RESOLVED"],
+          default: "OPEN"
+        }
 }]
 });
 
